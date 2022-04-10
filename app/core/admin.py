9034e8ea574765c11898664dbb89be25eec53cde
Ughur Guliyev <ugurguliyev24@gmail.com>
2022-04-11 00:38:53 +0400

    Admin page updated

--- app/core/admin.py
+++ app/core/admin.py
@@ -5,6 +5,6 @@ from core.models import News
 
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('headline', 'source_url', 'publication_date')
+    list_display = ('headline', 'source_url', 'publication_date', 'trend_name')
     list_filter = ('publication_date',)
     search_fields = ('headline',)

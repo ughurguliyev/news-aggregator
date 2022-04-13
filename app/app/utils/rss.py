5f63a72f9d47c238479655b0db5676f0c09fedfc
Ughur Guliyev <ugurguliyev24@gmail.com>
2022-04-13 16:54:57 +0400

    Headers updated

--- app/app/utils/rss.py
+++ app/app/utils/rss.py
@@ -6,10 +6,21 @@ class ReadRss:
         Read Rss from given source
     """
     headers = {
-        "User-Agent": "Mozilla/5.0",
-        "Accept": "*/*",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
         "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Alt-Used": "www.segodnya.ua",
+        "Cache-Control" : "max-age=0",
         "Connection": "keep-alive",
+        "Cookie": "_ga_WWD3WW3WCP=GS1.1.1649838491.11.0.1649838491.0; _ga=GA1.1.124747383.1649416465; __io_lv=1649579542940; __io=8cb71f4a5.5dee62cc6_1649416466730; __gfp_64b=2qQlkCqCuHOijL2DNzKTp8A7pma1MmVA5QS6Oy4UVML.i7|1649416467; _gid=GA1.2.973461271.1649790206",
+        "Host": "www.segodnya.ua",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-User": "?1",
+        "TE": "trailers",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:99.0) Gecko/20100101 Firefox/99.0"
     }
  
     def __init__(self, rss_url):
